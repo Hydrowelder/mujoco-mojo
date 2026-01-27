dcb8d4c903cf75af374a3427f2cf311ca44a3bdd
David Gable <david@david-gable.com>
2026-01-26 18:28:11 -0800

    Initial commit

---
+++ src/mujoco_mojo/utils.py
@@ -0,0 +1,27 @@
+from enum import StrEnum, auto
+
+
+class Color(StrEnum):
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    DARKCYAN = "\033[97m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    RED = "\033[91m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    ITALIC = "\033[3m"
+    NC = "\033[0m"
+
+
+class SimStatus(StrEnum):
+    RUNNING = auto()
+    NORMAL_TERMINATION = auto()
+    ERROR_TERMINATION = auto()
+
+    @classmethod
+    def prog_bar(cls, p: float, width: int = 40) -> str:
+        p = min(max(0, p), 1)
+        filled_length = int(width * p)
+        return f"[{'█' * filled_length}{'░' * (width - filled_length)}]"
