4bd1eb67a6585599faa6ec0d022101a01f1a9dc3
David Gable <david@david-gable.com>
2026-01-26 21:20:23 -0800

    Added compiler

--- src/mujoco_mojo/mjcf/mujoco.py
+++ src/mujoco_mojo/mjcf/mujoco.py
@@ -10,11 +10,48 @@ __all__ = ["Mujoco"]
 
 
 class Mujoco(XMLModel):
+    """The unique top-level element, identifying the XML file as an MJCF model file."""
+
     tag = "mujoco"
 
     attributes = {"model"}
-    children_map = {"worldbody": "worldbody", "options": "options"}
+    children_map = {
+        "options": "options",
+        "compilers": "compilers",
+        "sizes": "sizes",
+        "statistics": "statistics",
+        "assets": "assets",
+        "worldbody": "worldbody",
+        "deformables": "deformables",
+        "contacts": "contacts",
+        "equalities": "equalities",
+        "tendons": "tendons",
+        "actuators": "actuators",
+        "sensors": "sensors",
+        "keyframes": "keyframes",
+        "visuals": "visuals",
+        "defaults": "defaults",
+        "customs": "customs",
+        "extensions": "extensions",
+    }
 
     model: str = "MuJoCo Model"
+    """The name of the model. This name is shown in the title bar of simulate.cc."""
+
     options: List[Option] = []
+    compilers: List = []
+    sizes: List = []
+    statistics: List = []
+    assets: List = []
     worldbody: Optional[WorldBody] = None
+    deformables: List = []
+    contacts: List = []
+    equalities: List = []
+    tendons: List = []
+    actuators: List = []
+    sensors: List = []
+    keyframes: List = []
+    visuals: List = []
+    defaults: List = []
+    customs: List = []
+    extensions: List = []
