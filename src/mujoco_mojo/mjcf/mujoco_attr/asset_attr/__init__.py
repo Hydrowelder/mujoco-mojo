41482d5427e13d4fab4149fa23853bbb58e8f20f
David Gable <david@david-gable.com>
2026-01-27 21:04:36 -0800

    Added material Layer

--- src/mujoco_mojo/mjcf/mujoco_attr/asset_attr/__init__.py
+++ src/mujoco_mojo/mjcf/mujoco_attr/asset_attr/__init__.py
@@ -1,3 +1,5 @@
+from mujoco_mojo.mjcf.mujoco_attr.asset_attr.material_attr import *
+from mujoco_mojo.mjcf.mujoco_attr.asset_attr.mesh_attr import *
 from mujoco_mojo.mjcf.mujoco_attr.asset_attr.hfield import *
 from mujoco_mojo.mjcf.mujoco_attr.asset_attr.material import *
 from mujoco_mojo.mjcf.mujoco_attr.asset_attr.mesh import *
