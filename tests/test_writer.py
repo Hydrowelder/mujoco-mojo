d13886ee35eed49305ca070f69b20b97101b671a
David Gable <david@david-gable.com>
2026-01-30 21:58:07 -0800

    Added rotation object conversion

--- tests/test_writer.py
+++ tests/test_writer.py
@@ -14,54 +14,59 @@ def test_import():
     mjcf.__name__
 
 
+# =============== build a basic model ===============
 quat = mjcf.Quat(quat=np.array([1, 2, 3, 4]))
 sphere = mjcf.GeomSphere(size=0.2, rgba=np.asarray((1, 0, 0, 1)))
 material = mjcf.Material(name=mojo.typing.MaterialName("material_name"))
 
-world = mjcf.WorldBody(
-    geoms=[
-        mjcf.GeomPlane(
-            name="floor",
-            size=np.asarray((5, 5, 0.1)),
-            rgba=np.array((0.5, 0.5, 0.5, 1)),
-            pos=mjcf.Pos(pos=np.array((1, 2, 3))),
-            orientation=quat,
-            material=material.name,
-        )
-    ],
-    bodies=[
-        mjcf.Body(
-            name=mojo.typing.BodyName("robot"),
-            geoms=[
-                sphere,
-                mjcf.GeomCylinder(
-                    size=np.asarray([1, 3]),
-                    rgba=np.asarray((1, 0, 0, 1)),
-                ),
-            ],
-        )
-    ],
-)
-
 model = mjcf.Mujoco(
-    worldbody=world, model="hello", compilers=[mjcf.Compiler(balanceinertia=True)]
+    worldbody=mjcf.WorldBody(
+        geoms=[
+            mjcf.GeomPlane(
+                name="floor",
+                size=np.asarray((5, 5, 0.1)),
+                rgba=np.array((0.5, 0.5, 0.5, 1)),
+                pos=mjcf.Pos(pos=np.array((1, 2, 3))),
+                orientation=quat,
+                material=material.name,
+            )
+        ],
+        bodies=[
+            mjcf.Body(
+                name=mojo.typing.BodyName("robot"),
+                geoms=[
+                    sphere,
+                    mjcf.GeomCylinder(
+                        size=np.asarray([1, 3]),
+                        rgba=np.asarray((1, 0, 0, 1)),
+                    ),
+                ],
+            )
+        ],
+    ),
+    model="hello",
+    compilers=[mjcf.Compiler(balanceinertia=True)],
 )
 
-# ensure it works with mujoco
+
+# =============== ensure it works with mujoco ===============
 xml = mojo.utils.to_pretty_xml(model.to_xml())
 save_as = Path(__file__).with_name("result_test_writer.xml")
 save_as.write_text(xml)
 
 m = mujoco.MjSpec.from_file(save_as.as_posix())
 
-# serialize and deserialize
+
+# =============== serialize and deserialize ===============
 json_file = save_as.with_suffix(".json")
 
+# quaternion (this is a discriminated type)
 json_file.with_stem("quat").write_text(quat.model_dump_json(exclude_none=True))
 assert "type" in quat.model_dump_json(exclude_none=True), (
     "Orientation type was not serialized"
 )
 quat = mjcf.Quat.model_validate_json(json_file.with_stem("quat").read_text())
+
 
 json_file.with_stem("sphere").write_text(sphere.model_dump_json(exclude_none=True))
 assert "type" in sphere.model_dump_json(exclude_none=True), (
