736e39cc92d49ad009cec9b0357266df99156730
David Gable <david@david-gable.com>
2026-02-22 16:39:16 -0800

    Adding monte carlo functionality

--- src/mujoco_mojo/base.py
+++ src/mujoco_mojo/base.py
@@ -1,5 +1,27 @@
-from pydantic import BaseModel
+from pathlib import Path
+
+from pydantic import BaseModel, Field
+
+from mujoco_mojo.mjcf.mujoco import Mujoco
+from mujoco_mojo.process_manager import NamedValueDict, NamedValueList
 
 
 class MojoBaseModel(BaseModel):
     """Base model for all MuJoCo Mojo classes."""
+
+
+class Config(MojoBaseModel):
+    """Contains metadata definitions for how jobs should be run."""
+
+    workdir: Path
+    iterations: int
+
+
+class Mojo(MojoBaseModel):
+    """Mojo is the highest level watcher which manages running jobs."""
+
+    mjcf: Mujoco  # contains Var which reference to named_value
+    named_values: NamedValueDict | NamedValueList = Field(
+        NamedValueDict()
+    )  # contains named values
+    config: Config
---
+++ src/mujoco_mojo/process_manager/__init__.py
@@ -0,0 +1,34 @@
+from process_manager import (
+    BernoulliDistribution,
+    CategoricalDistribution,
+    ExponentialDistribution,
+    LogNormalDistribution,
+    NamedValue,
+    NamedValueDict,
+    NamedValueList,
+    NamedValueState,
+    NormalDistribution,
+    PoissonDistribution,
+    TriangularDistribution,
+    TruncatedNormalDistribution,
+    UniformDistribution,
+)
+
+from .var import Var
+
+__all__ = [
+    "BernoulliDistribution",
+    "CategoricalDistribution",
+    "ExponentialDistribution",
+    "LogNormalDistribution",
+    "NamedValue",
+    "NamedValueDict",
+    "NamedValueList",
+    "NamedValueState",
+    "NormalDistribution",
+    "PoissonDistribution",
+    "TriangularDistribution",
+    "TruncatedNormalDistribution",
+    "UniformDistribution",
+    "Var",
+]
