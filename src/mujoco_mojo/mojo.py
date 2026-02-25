28fde1ffac10281a86feae2a16d0a10395abcbc8
David Gable <dave.a.gable@gmail.com>
2026-02-24 20:09:53 -0800

    Added a validator to assure all Val references are made to the Mojo class

--- src/mujoco_mojo/mojo.py
+++ src/mujoco_mojo/mojo.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Self
 
 from process_manager.distribution import DistributionDict
-from pydantic import Field
+from pydantic import Field, model_validator
 
 from mujoco_mojo.base import MojoBaseModel
 from mujoco_mojo.mjcf.mujoco import Mujoco
-from mujoco_mojo.process_manager import NamedValueDict
+from mujoco_mojo.process_manager import NamedValueDict, ValueName
 
 
 class Config(MojoBaseModel):
@@ -28,3 +29,29 @@ class Mojo(MojoBaseModel):
     mjcf: Mujoco
     values: Values = Field(default_factory=Values)
     config: Config
+
+    @model_validator(mode="after")
+    def validate_reported_named_values(self) -> Self:
+        unreported = self.unreported_named_values()
+        if unreported:
+            raise ValueError(
+                f"Not all named values in the MJCF were reported to Mojo. Offending values ({', '.join(unreported)})"
+            )
+        return self
+
+    def unreported_named_values(self) -> list[ValueName]:
+        """
+        Recursively searches the NamedValue references in the MJCF and compares to the NamedValue reported to Mojo.
+
+        Returns:
+            list[ValueName]: All ValueNames in the MJCF that were not found in the reported NamedValueDict.
+
+        """
+        in_mjcf = set([v.ref for v in self.mjcf.vals])
+        reported = set(self.values.named.keys())
+
+        unreported = []
+        for m in in_mjcf:
+            if m not in reported:
+                unreported.append(m)
+        return unreported
