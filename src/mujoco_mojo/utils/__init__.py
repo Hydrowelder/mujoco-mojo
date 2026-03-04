4e349959d11b85c1fcefbce63c6d8c3af41bba31
David Gable <dave.a.gable@gmail.com>
2026-03-03 21:24:06 -0800

    Add __init__.py

--- src/mujoco_mojo/utils/__init__.py
+++ src/mujoco_mojo/utils/__init__.py
@@ -1,6 +1,4 @@
 from .runner import MojoGenerator, MojoRunner, MojoRuntime, MonteCarloConfig, Trial
-
-# from .statusing import Status
 from .utils import Color, is_empty_list, to_pretty_xml
 
 __all__ = [
@@ -9,7 +7,6 @@ __all__ = [
     "MojoRunner",
     "MojoRuntime",
     "MonteCarloConfig",
-    # "Status",
     "Trial",
     "is_empty_list",
     "to_pretty_xml",
