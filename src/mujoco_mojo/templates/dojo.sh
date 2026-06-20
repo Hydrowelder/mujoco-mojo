c00f88fb4017d565e81a1025360869694ffcdb37
David Gable <dave.a.gable@gmail.com>
2026-06-19 17:18:47 -0700

    Fixed a windows bug where running dojo.sh prevented workdir cleanup on a rerun

--- src/mujoco_mojo/templates/dojo.sh
+++ src/mujoco_mojo/templates/dojo.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-mujoco-mojo dojo .
+# exec replaces this shell process with the dojo server instead of forking it as a
+# child; on Windows (Git Bash/MSYS), bash otherwise keeps this script file checked out
+# for as long as the long-running dojo server is alive, which blocks deleting/replacing
+# the workdir on a rerun
+exec mujoco-mojo dojo .
